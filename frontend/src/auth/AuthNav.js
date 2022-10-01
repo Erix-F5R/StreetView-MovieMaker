148096ac597252d496e956d8ddff48a03d1ff03f
Eric <ericpeter.f@gmail.com>
2022-10-01 17:41:24 -0400

    upgrade ubuntu

---
+++ frontend/src/auth/AuthNav.js
@@ -0,0 +1,12 @@
+// src/components/auth-nav.js
+
+import React from 'react';
+import AuthenticationButton from './AuthenticationButton';
+
+const AuthNav = () => (
+  <div>
+    <AuthenticationButton />
+  </div>
+);
+
+export default AuthNav;
