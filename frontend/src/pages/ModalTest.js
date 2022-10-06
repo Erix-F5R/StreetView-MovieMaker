ba8e00c7a33e07b1a699c0cd46a350a03060b82c
Eric <ericpeter.f@gmail.com>
2022-10-06 13:54:39 -0400

    modal reducer init

--- frontend/src/pages/ModalTest.js
+++ frontend/src/pages/ModalTest.js
@@ -6,16 +6,49 @@ import useInterval from "../hooks/useInterval";
 
 const ModalTest = () => {
   const [open, setOpen] = useState(false);
+  const [count, setCount] = useState(0);
+  const [play, setPlay] = useState("Play");
+
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
-  const images = ['a','b','c','d','e','f','g','h']
+  const handleClose = () => {
+    setCount(0);
+    setOpen(false);
+    setPlay("Play");
+  };
 
-  let [count, setCount] = useState(0);
+  const handlePlayPauseReset = () => {
+    if (play === "Play") {
+      setPlay("Pause");
+    } else if (play === "Pause") {
+      setPlay("Play");
+    } else if (play === "Reset") {
+      setPlay("Pause");
+    }
+  };
 
-  useInterval(() => {
-    setCount(count + 1);
-  }, 1000);
+  const images = [
+    "streetview.jpeg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
+    "11.jpg",
+    "12.jpg",
+  ];
+
+  useInterval(
+    () => {
+      count === images.length - 1 ? setPlay("Reset") : setCount(count + 1);
+    },
+    play === "Pause" ? 1000 : null
+  );
 
   return (
     <>
@@ -27,20 +60,33 @@ const ModalTest = () => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Div>{images[count % (images.length) ]}</Div>
+        <Container>
+          <img
+            style={{ height: "400px", width: "400px" }}
+            src={require("../assets/" + `${images[count % images.length]}`)}
+          />
+          <ButtonWrapper>
+          <button onClick={handlePlayPauseReset}>{play}</button>
+          <button>Rate This Trip</button>
+          </ButtonWrapper>
+        </Container>
       </Modal>
     </>
   );
 };
 
-const Div = styled.div`
+const Container = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
+  padding: 10px;
+  background: white;
+  border-radius: 5px;
   transform: translate(-50%, -50%);
-  width: 400px;
   border: 2px solid #000;
   box-shadow: 24;
 `;
+
+const ButtonWrapper = styled.div``;
 
 export default ModalTest;
